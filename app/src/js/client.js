287809e083916c489da964652d3d5fd156f9af7f
sin-sky-sora <sinskyozz@gmail.com>
2021-12-27 01:39:12 +0900

    app folder

---
+++ app/src/js/client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello wolrd!</h1>
+      <h4>Do you see it?</h4>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ app/webpack.config.js
@@ -0,0 +1,36 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  context: path.join(__dirname, "src"),
+  entry: "./js/client.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "src"),
+    filename: "client.min.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    port: 8080,
+  },
+  plugins: debug ? [] : [new webpack.optimize.OccurrenceOrderPlugin(), new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })],
+};
